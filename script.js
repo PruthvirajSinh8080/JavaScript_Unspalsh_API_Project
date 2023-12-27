b111346a24e456c8a954f07eab0ef8a33cf86481
Pruthvirajsinh Jadeja <154883234+PruthvirajSinh8080@users.noreply.github.com>
2023-12-27 13:11:07 +0530

    Update script.js because api environment variable

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const accessKey = process.env.UNSPLASH_API_KEY;
+const accessKey = "2gXvqEGis0819mW_b2mydqGACbfyk9gOCHtIurK4JRQ";
 
 const form = document.querySelector("form");
 const input = document.getElementById("search-input");
