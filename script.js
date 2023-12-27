b4178d16907f9bf5f695c454fc78a6e7b78a3024
Pruthvirajsinh Jadeja <154883234+PruthvirajSinh8080@users.noreply.github.com>
2023-12-27 12:58:02 +0530

    Update The mistake in Access key.env script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const accessKey = process.env.UNSPLASH_API_KEY;
+const accessKey = key.env.UNSPLASH_API_KEY;
 
 const form = document.querySelector("form");
 const input = document.getElementById("search-input");
